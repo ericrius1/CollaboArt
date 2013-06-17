69cd063f8ecce3f4ffa19588bb57503d737699b1
Eric <Ericrius1@gmail.com>
2013-06-17 16:50:57 -0700

    got basic particle moving

--- public/js/world.js
+++ public/js/world.js
@@ -1,16 +1,36 @@
-var world = function(p5){
-  console.log("yo");
-  p5.setup = function(){
+var world = function(p5) {
+  var x, y;
+  var vx, vy;
+  p5.setup = function() {
     p5.size(window.innerWidth, window.innerHeight, p5.P3D);
+    p5.resetParticle();
   }
 
-  p5.draw = function(){
+  p5.draw = function() {
     p5.background(0)
+    p5.ellipse(x, y, 10, 10)
+
+    x +=vx;
+    y += vy;
+  }
+
+  p5.resetParticle = function(event) {
+    console.log("shnur")
+    x = y = p5.width / 2.0;
+
+    //set new random velocity
+    // b/w - and + numbers
+    vx = p5.random(-0.39, 0.1);
+    vy = p5.random(-0.1, 0.21); 
+
   }
 }
 
 
-$(document).ready(function(){
+$(document).ready(function() {
   canvas = document.getElementById('processing');
-  processing= new Processing(canvas, world);
+  processing = new Processing(canvas, world);
+  $('#processing').on('mousedown', function(event) {
+    processing.resetParticle(event);
+  })
 });
