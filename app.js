162b9a01de3ed00512449fb94dd1923ba7aed67f
Eric <Ericrius1@gmail.com>
2013-06-16 22:07:33 -0700

    added socket io

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.logger());
 
 console.log('listen on port 3000');
 app.listen(3000);
+
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
