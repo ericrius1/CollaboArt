2e78fe0761f9bf6001507b1560939476a09f9831
Eric <Ericrius1@gmail.com>
2013-06-17 17:25:23 -0700

    dot goes where user clicks

--- public/js/world.js
+++ public/js/world.js
@@ -3,26 +3,19 @@ var world = function(p5) {
   var vx, vy;
   p5.setup = function() {
     p5.size(window.innerWidth, window.innerHeight, p5.P3D);
-    p5.resetParticle();
+    x = 300;
+    y = 300;
   }
 
   p5.draw = function() {
     p5.background(0)
     p5.ellipse(x, y, 10, 10)
-
-    x +=vx;
-    y += vy;
   }
 
   p5.resetParticle = function(event) {
-    console.log("shnur")
-    x = y = p5.width / 2.0;
-
-    //set new random velocity
-    // b/w - and + numbers
-    vx = p5.random(-0.39, 0.1);
-    vy = p5.random(-0.1, 0.21); 
-
+    x = event.clientX;
+    y = event.clientY;
+    
   }
 }
 
