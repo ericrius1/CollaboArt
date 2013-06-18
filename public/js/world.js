fa253921d9ce57ac2d4bfb0be6e166a94c8780c9
Eric <Ericrius1@gmail.com>
2013-06-18 10:11:27 -0700

    more experiments with webgl

--- public/js/world.js
+++ public/js/world.js
@@ -1,34 +1,115 @@
-var world = function(processing) {
-  p = processing;
-  var ps;
-  var comm = new Comm();
-  comm.listen();
-  p.setup = function() {
-    p.size(window.innerWidth, window.innerHeight, p.P3D);
-    ps = new ParticleSystem();
-    ps.init();
-  }
+var World = function() {
+  
+ var container, stats;
 
-  p.draw = function() {
-    p.background(0)
-    ps.run();
-    
-  }
+      var camera, scene, renderer;
 
-  p.moveParticle = function(event) {
-    comm.clicked(event);
-  }
+      var mesh, zmesh, lightMesh, geometry;
 
-  p.update = function(position){
-    
-  }
+      var mouseX = 0, mouseY = 0;
+
+      var windowHalfX = window.innerWidth / 2;
+      var windowHalfY = window.innerHeight / 2;
+
+      document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
+      init();
+      animate();
+
+
+      function init() {
+
+        container = document.createElement( 'div' );
+        document.body.appendChild( container );
+
+        camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 10000 );
+        camera.position.set( 100000, 0, 3200 );
+
+        scene = new THREE.Scene();
+
+        var colors = [ 0x000000, 0xff0080, 0x8000ff, 0xffffff ];
+        var geometry = new THREE.Geometry();
+
+        for ( var i = 0; i < 2000; i ++ ) {
+
+          var vertex = new THREE.Vector3();
+          vertex.x = Math.random() * 4000 - 2000;
+          vertex.y = Math.random() * 4000 - 2000;
+          vertex.z = Math.random() * 4000 - 2000;
+          geometry.vertices.push( vertex );
+
+          geometry.colors.push( new THREE.Color( colors[ Math.floor( Math.random() * colors.length ) ] ) );
+
+        }
+
+        var material = new THREE.ParticleBasicMaterial( { size: 1, vertexColors: THREE. VertexColors, depthTest: false, opacity: 0.5, sizeAttenuation: false, transparent: true } );
+
+        var mesh = new THREE.ParticleSystem( geometry, material );
+        scene.add( mesh );
+
+        renderer = new THREE.WebGLRenderer( { preserveDrawingBuffer: true } );
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        renderer.sortObjects = false;
+        renderer.autoClearColor = false;
+        container.appendChild( renderer.domElement );
+
+        stats = new Stats();
+        stats.domElement.style.position = 'absolute';
+        stats.domElement.style.top = '0px';
+        stats.domElement.style.zIndex = 100;
+        container.appendChild( stats.domElement );
+
+        //
+
+        window.addEventListener( 'resize', onWindowResize, false );
+
+      }
+
+      function onWindowResize() {
+
+        windowHalfX = window.innerWidth / 2;
+        windowHalfY = window.innerHeight / 2;
+
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize( window.innerWidth, window.innerHeight );
+
+      }
+
+      function onDocumentMouseMove(event) {
+
+        mouseX = ( event.clientX - windowHalfX ) * 10;
+        mouseY = ( event.clientY - windowHalfY ) * 10;
+
+      }
+
+      //
+
+      function animate() {
+
+        requestAnimationFrame( animate );
+
+        render();
+        stats.update();
+
+      }
+
+      function render() {
+
+        camera.position.x += ( mouseX - camera.position.x ) * .05;
+        camera.position.y += ( - mouseY - camera.position.y ) * .05;
+
+        camera.lookAt( scene.position );
+
+        renderer.render( scene, camera );
+
+      }
+ 
+ 
 }
 
 
-$(document).ready(function() {
-  canvas = document.getElementById('processing');
-  processing = new Processing(canvas, world);
-  $('#processing').on('mousedown', function(event) {
-    processing.moveParticle(event);
-  })
-});
+
+
+
